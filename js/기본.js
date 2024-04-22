a791c84a9747b4ee08dfb7e1af559e193a5864ef
user1 <dlaeocjf94@naver.com>
2024-04-22 12:37:59 +0900

    자바스크립트 기본형 연습

--- js/기본.js
+++ js/기본.js
@@ -4,9 +4,43 @@
 
 // 변수는 값을 여러번 대입 할 수 있음 : let, var
 
-let x = 10;
-console.log('x의 값은 : ',x); // 20
+// let x = 10;
+// console.log('x의 값은 : ',x); // 20
 
-var y = 20;
-y = 30;
-console.log('y의 값은 : ', y);
+// var y = 20;
+// y = 30;
+// console.log('y의 값은 : ', y);
+
+// 데이터 타입 
+// js는 동적 타입의 언어이지만 내부적으로 데이터 타입이 존재
+// 원시 타입(할당 받은 공간에 값이 대입)
+// 객체 타입(주소값을 가리킴)이 존재
+
+// js에서 문자열을 표시하는 방법 : '',"",``
+// ``(백틱)은 ES6에서 추가된 문법이고 `${변수 또는 코드}`를 포함 할 수 있음
+
+let str = "안녕하다.\n" + ' 자바스크립트 배운다.\t\t' + `백틱도 문자열 가능`;
+console.log(str);
+
+// 탬플릿 문자열 : ES6에서 추가된 문자열을 정의하는 방법으로 백틱이라고 부름
+let dan = 3;
+let gugu = 8;
+console.log(`${dan} 곱하기 ${gugu}은 ${dan * gugu}이다. `);
+
+// 숫자형(Number) : js는 정수와 실수를 구분하지 않고 숫자형으로 취급
+let num1 = 10;
+let num2 = 0.1;
+console.log(num1 + num2);
+
+console.log(0.1+ 0.2);
+
+ // toString() : 숫자형 데이터를 문자형 데이터로 변환해주는 함수
+ let x = 123;
+ console.log(typeof x.toString());
+
+ // 논리형 : true or false 구분
+ let age = 10;
+ let isAdult = (age > 18) ? true : false;
+ if(isAdult)
+ console.log('성인!');
+else console.log('애기');
