95a6cd6f052d4aec7520f0c3222624697201c4f4
user1 <dlaeocjf94@naver.com>
2024-04-23 15:39:06 +0900

    객체 공부1

---
+++ js/객체.js
@@ -0,0 +1,11 @@
+// js에선 원시타입을 제외한 모든 값이 참조 타입임
+// 객체 : 중괄호를 사용해서 만들며 속성(property)과 값의 쌍으로 구성
+const person = {
+  name : "안유진",
+  age : 21,
+  addr : '서울시 강남구 일원동',
+  group : "아이브",
+  desc : function(){
+    return "아이브"
+  }
+};
