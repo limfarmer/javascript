cba7ac32c18766153d77b27091abe965e314f1ea
user1 <dlaeocjf94@naver.com>
2024-04-23 14:02:12 +0900

    콜백함수 연습

--- js/스코프.js
+++ js/스코프.js
@@ -29,14 +29,28 @@
 // console.log(`블록 내부 ${b}`);
 
 // 참조 우선 순위 : 블록 안에 변수가 우선 순위
-let a = 10;
-const b = 20;
+// let a = 10;
+// const b = 20;
 
-function sum20(){
-  let a =50;
-  const b = 70;
-  console.log(`함수 내부 ${a}`);
-  console.log(`함수 내부 ${b}`);
+// function sum20(){
+//   let a =50;
+//   const b = 70;
+//   console.log(`함수 내부 ${a}`);
+//   console.log(`함수 내부 ${b}`);
+// }
+// console.log(`함수 내부 ${a}`);
+// console.log(`함수 내부 ${b}`);
+
+// 즉시 실행 함수 : 함수를 정의하자마자 바로 실행하는 함수
+(function(){
+  width = 500;
+  height = 300;
+  console.log(`width : ${width} height : ${height}`);
+  init();
+})();
+
+function init(){
+  console.log(`프로글매 실행을 위해서 초기화 작업 수행중`);
 }
-console.log(`함수 내부 ${a}`);
-console.log(`함수 내부 ${b}`);
+
+
